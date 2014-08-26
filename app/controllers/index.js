fe681729d4cc14a6ced143a267cbd60161b5a286
Tim Stirrat <tim.stirrat@gmail.com>
2014-08-26 10:05:01 -0700

    Auto increment comment bodies

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,9 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.ObjectController.extend({
+
+  commentIndex: 1,
+
   actions: {
     go: function () {
-      var c = this.store.createRecord('comment', { body: 'comment 2' });
+      this.incrementProperty('commentIndex');
+      var c = this.store.createRecord('comment', { body: 'comment ' + this.get('commentIndex') });
       this.get('model.comments').pushObject(c);
       this.get('model').save();
     }
