13ce7a41ebc027350081458518e77fe6a2cfe7c9
Tim Stirrat <tim.stirrat@gmail.com>
2014-08-26 09:51:26 -0700

    Add ClientIdEmbeddedRecordsMixin

--- app/serializers/application.js
+++ app/serializers/application.js
@@ -1,5 +1,6 @@
 import DS from 'ember-data';
+import ClientIdEmbeddedRecordsMixin from '../mixins/client-id-embedded-records';
 
-export default DS.ActiveModelSerializer.extend({
+export default DS.ActiveModelSerializer.extend(ClientIdEmbeddedRecordsMixin, {
 
 });
