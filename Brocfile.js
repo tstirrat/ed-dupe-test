d75625814f6f52fcacec7d085f53357483047628
Tim Stirrat <tim.stirrat@gmail.com>
2014-11-10 15:25:08 -0800

    Fix bower imports

--- Brocfile.js
+++ Brocfile.js
@@ -4,7 +4,7 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 var app = new EmberApp();
 
-app.import('vendor/bootstrap/dist/css/bootstrap.min.css');
-app.import('vendor/bootstrap/dist/css/bootstrap.css.map');
+app.import('bower_components/bootstrap/dist/css/bootstrap.min.css');
+app.import('bower_components/bootstrap/dist/css/bootstrap.css.map');
 
 module.exports = app.toTree();
